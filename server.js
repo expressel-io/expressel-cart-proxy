f50257e3a03d8452834d4dc20a036d3566130417
Katie Truong <katietruong.ucdavis@gmail.com>
2018-08-01 15:44:51 -0700

    Update proxy server

--- server.js
+++ server.js
@@ -3,20 +3,13 @@ const morgan = require('morgan');
 const path = require('path');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 4000;
-const redirectedPort = 3003;
+const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
 app.use(cors());
 
 app.use('/item/:id', express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
-
-//TODO: Redirect 4000 to 3003
-const handleRedirect = function(req, res) {
-  const targetUrl = targetBaseUrl + req.originalUrl;
-  res.redirect(targetUrl);
-}
 
 
 app.listen(port, () => {
